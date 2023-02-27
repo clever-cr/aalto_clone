3cd3d614179ff24a36f11dd2b19006a2edcd9851
clever-cr <cleverumuhuza@gmail.com>
2023-02-27 18:45:25 +0200

    fix issues

--- components/sections/Explore.jsx
+++ components/sections/Explore.jsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import Image from "next/image";
 import CommunityCard from "../ui/QuickLink";
 import QuickLink from "../ui/QuickLink";
+import Wrapper from "../layout/Wrapper";
 const Explore = () => {
   const links = [
     {
@@ -24,42 +25,50 @@ const Explore = () => {
   ];
   return (
     <div className="pt-[72px]">
-      <div>
-        <Image
-          src={rautio}
-          alt=""
-          className="h-[400px] sm:w-[483px] object-cover"
-        />
-        <div className="px-[80px] relative -mt-[209px] sm:px-5">
-          <div className="bg-yellow  py-[40px]">
-            <h2 className="text-[75px] leading-[88px] pb-[81px] px-[40px] sm:text-[30px] sm:leading-[36px] font-bold">
-              Explore Aalto University
-            </h2>
-            <div className="flex border-t justify-between px-[141px] sm:border-t-0  sm:flex-col sm:px-5 ">
-              {["Study at Aalto", "Open positions", "Contact us"].map(
-                (link, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className={`border-r pr-[142px] py-[30px] sm:pr-0 sm:border-r-0 sm:border-t sm:py-[24px] sm:text-center ${
-                        index == 2 ? "border-r-0" : ""
-                      }`}
-                    >
-                      <Link
-                        className="text-[28px] leading-[33px] text-gray underline "
-                        href="#"
-                      >
-                        {link}
-                      </Link>
-                    </div>
-                  );
-                }
-              )}
+      <Wrapper>
+        <div>
+          <div>
+            <Image
+              src={rautio}
+              alt=""
+              className="h-[400px] sm:w-[483px] object-cover"
+            />
+            <div className="px-[80px] relative -mt-[209px] sm:px-5">
+              <div className="bg-yellow  py-[40px]">
+                <h2 className="text-[75px] leading-[88px] pb-[81px] px-[40px] sm:text-[30px] sm:leading-[36px] font-bold">
+                  Explore Aalto University
+                </h2>
+                <div className="flex border-t justify-between px-[141px] sm:border-t-0  sm:flex-col sm:px-5 ">
+                  {["Study at Aalto", "Open positions", "Contact us"].map(
+                    (link, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={`border-r pr-[142px] py-[30px] sm:pr-0 sm:border-r-0 sm:border-t sm:py-[24px] sm:text-center ${
+                            index == 2 ? "border-r-0" : ""
+                          }`}
+                        >
+                          <Link
+                            className="text-[28px] leading-[33px] text-gray underline "
+                            href="#"
+                          >
+                            {link}
+                          </Link>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              </div>
             </div>
           </div>
+          <QuickLink
+            title="Important quicklinks "
+            content={links}
+            hasIcon={true}
+          />
         </div>
-      </div>
-      <QuickLink title="Important quicklinks " content={links} hasIcon={true} />
+      </Wrapper>
     </div>
   );
 };
--- components/sections/Schools.jsx
+++ components/sections/Schools.jsx
@@ -3,6 +3,7 @@ import unto from "/public/images/unto.jpeg";
 import Link from "next/link";
 import Image from "next/image";
 import InformationCard from "../ui/InformationCard";
+import Wrapper from "../layout/Wrapper";
 const Schools = () => {
   const data = [
     {
@@ -38,14 +39,18 @@ const Schools = () => {
   ];
   return (
     <div className="px-[80px] space-y-[16px] sm:px-5">
-      <h2 className="text-[28px] leading-[33px] text-gray">
-        Explore our six schools
-      </h2>
-      <p className=" text-lg leading-[26px] text-gray">
-        Scientific research and artistic activities are carried out at six
-        schools and their departments and units.
-      </p>
-      <InformationCard datas={data} />
+      <Wrapper>
+        <div>
+          <h2 className="text-[28px] leading-[33px] text-gray">
+            Explore our six schools
+          </h2>
+          <p className=" text-lg leading-[26px] text-gray">
+            Scientific research and artistic activities are carried out at six
+            schools and their departments and units.
+          </p>
+          <InformationCard datas={data} />
+        </div>
+      </Wrapper>
     </div>
   );
 };
